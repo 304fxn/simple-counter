2b1ed20f5a8e893e85cf3b2316cec537bbbddad6
Florian Neacsu <78438463+304fxn@users.noreply.github.com>
2021-10-30 22:32:48 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const btncount = document.querySelector(".btncount");
+const btndecrease = document.querySelector(".btndecrease");
+const btnreset = document.querySelector(".btnreset");
+const btnincrease = document.querySelector(".btnincrease");
+
+let count = 0;
+
+btnincrease.addEventListener("click", function () {
+  count++;
+  btncount.innerHTML = count;
+});
+btndecrease.addEventListener("click", function () {
+  if (count == 0) {
+    console.log("Nu poti micsora numarul!");
+  } else {
+    count--;
+    btncount.innerHTML = count;
+  }
+});
+btnreset.addEventListener("click", function () {
+  count = 0;
+  btncount.innerHTML = count;
+});
